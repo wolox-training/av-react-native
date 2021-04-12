23235e31c624bd672850f813a62120875abb1574
androsv96 <androsg93@hotmail.com>
2021-04-12 13:38:45 -0500

    fixed PR suggestions

--- wbooks/App.tsx
+++ wbooks/App.tsx
@@ -3,6 +3,5 @@ import React from 'react';
 import App from './src/app';
 
 export default function index() {
-  console.tron.log('hola');
   return <App />;
 }
--- wbooks/index.js
+++ wbooks/index.js
@@ -6,9 +6,6 @@ import { AppRegistry } from 'react-native';
 
 import App from './App';
 import { name as appName } from './app.json';
-
-if (__DEV__) {
-  import('./ReactotronConfig');
-}
+import('./src/config/reactotronConfig.ts');
 
 AppRegistry.registerComponent(appName, () => App);
