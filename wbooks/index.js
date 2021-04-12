f0df1aea6d3da054cef70ebf589ec0fdaf44d630
androsv96 <androsg93@hotmail.com>
2021-04-12 13:45:18 -0500

    uploading uncommited files.

--- wbooks/index.js
+++ wbooks/index.js
@@ -6,6 +6,7 @@ import { AppRegistry } from 'react-native';
 
 import App from './App';
 import { name as appName } from './app.json';
+
 import('./src/config/reactotronConfig.ts');
 
 AppRegistry.registerComponent(appName, () => App);
