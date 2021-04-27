4be26a9ec5fe021c5ef101647e7dfe7d6ab40781
androsv96 <androsg93@hotmail.com>
2021-04-27 13:16:16 -0500

    modified app header

---
+++ wbooks/src/app/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import { cerulean } from '@constants/colors';
+
+export default StyleSheet.create({
+  headerStyle: {
+    backgroundColor: cerulean,
+    height: 100
+  }
+});
